fa52eb493824787f3dc7af91cbdd699ac9fc38e0
Mauricio Goes <mauriciogoes29@gmil.com>
2022-07-01 16:13:59 -0300

    adicional de pericias funcional. Precisa acertar pontuação.

---
+++ js/grupoDePericias.js
@@ -0,0 +1,8 @@
+const grupoDePericias = {
+    combateCorpoACorpo:{id:"combateCorpoACorpo", nome:"Combate Corpo a Corpo", idAtributoBase:"atributoAtaque"},
+    combateADistancia:{id:"combateADistancia", nome:"Combate A Distancia", idAtributoBase:"atributoAtaque"},
+    ciencias:{id:"ciencias", nome:"Ciências", idAtributoBase:"atributoInteligencia"},
+    furtividade:{id:"furtividade", nome:"Furtividade", idAtributoBase:"atributoDestreza"},
+    manipulacao:{id:"manipulacao", nome:"Manipulação", idAtributoBase:"atributoCarisma"}
+}
+
--- js/pericias.js
+++ js/pericias.js
@@ -1,45 +1,89 @@
-let lerBtnPericias = document.querySelector("#btnAdicionarPericias");
-lerBtnPericias.addEventListener("click", function(){
-    console.log("Botão Pericias Clicado!");
-    adicionaPericiasALista();
+let lerBtnAddPericias = document.querySelector("#btnAdicionarPericias");
+lerBtnAddPericias.addEventListener("click", function(){    
+    criadorDeSelecaoDeListaDePericias();    
 });
 
-function adicionaPericiasALista(){
-    const selecionaIdListaDePericias = document.querySelector("#listaDePericias");
-    const periciaEscrita = escreverPericia();    
-    let escreverHtmlItemPericia = periciaHtml(periciaEscrita);
-    renderHtmlPericia(selecionaIdListaDePericias, escreverHtmlItemPericia);
-
+function criadorDeSelecaoDeListaDePericias(){
+    let importaGrupoDePericias = grupoDePericias;
+    let modeloHtmlSelect = criadorDeModeloHtml();        
+    let renderizaGrupoDePericias = rederizadorDeGrupoDeSelecaoDePericias(importaGrupoDePericias);
+    let btnAddAListaDePericias = document.querySelector("#btnAddAListaDePericias");
+    btnAddAListaDePericias.addEventListener("click", adicionadorDePericias);
 }
 
-function escreverPericia(){
-    let pericia = {
-        id:0,
-        nome:"Combate Corpo a Corpo(Laminas)",
-        atributoBase:"atributoAtaque",
-        atributoBaseValorTotal:0,
-        pontosInverstidos:0,
-        valorTotalDaPericia:0
-    }
-    return pericia;
-};
+function criadorDeModeloHtml(){
+    let selecionaDoContainerDeListaDeSelecao = document.querySelector("#listaDePericias");    
+    let modeloHtml = document.createElement("form");   
+    modeloHtml.innerHTML = `
+    <select id="listaDeSelecaoDePericias"><select>
+    <button id="btnAddAListaDePericias" type="button">Adicionar a Lista</button>
+    `;    
+    selecionaDoContainerDeListaDeSelecao.append(modeloHtml);    
+}
 
-function periciaHtml(periciaEscrita){
+function rederizadorDeGrupoDeSelecaoDePericias(importaGrupoDePericias){
+    let grupoDePericias = importaGrupoDePericias;
+    let selecionaDeListaDeSelecao = document.querySelector("#listaDeSelecaoDePericias");        
+    for(let contador = 0; contador < Object.keys(grupoDePericias).length; contador++){
+        let periciaDoGrupo = Object.keys(grupoDePericias)[contador];
+        let grupoOptionsPericias = document.createElement("option");
+        grupoOptionsPericias.innerHTML = grupoDePericias[`${periciaDoGrupo}`].nome;
+        grupoOptionsPericias.setAttribute("value", grupoDePericias[`${periciaDoGrupo}`].id);
+        selecionaDeListaDeSelecao.append(grupoOptionsPericias);
+    }
+}
+
+function adicionadorDePericias(){
+    let idDaPericia = document.querySelector("#listaDeSelecaoDePericias").value;
+    let selecionaListaDePericias = document.querySelector("#listaDePericiasPlayer");
+    let periciaChecada = checadorDeRepeticaoDePericias(idDaPericia);
+    if(periciaChecada === "erro"){
+        return;
+    }else{
+        selecionaListaDePericias.append(periciaChecada);
+    }    
+    leitorDeInputDaPericia(idDaPericia);
+}
+
+function checadorDeRepeticaoDePericias(idDaPericia){
+    let identificadorDaPericia = idDaPericia;
+    let modeloHtmlLi;
+    let localizadorDePericia = document.querySelector("#listaDePericiasPlayer").querySelector(`#${identificadorDaPericia}`)?.id;
+        
+    if(localizadorDePericia === identificadorDaPericia){
+        alert("Pericia Ja existe na lista escolha outra.");
+        let controle = "erro";
+        return controle;
+    } else if (localizadorDePericia !== identificadorDaPericia){        
+        modeloHtmlLi = criadorDeModeloHtmlLi(idDaPericia);
+    } else {
+        alert("Um erro ocorreu ao tentar localizar a pericia selecionada.");
+        return;
+    }
+    return modeloHtmlLi;
+}
+
+function criadorDeModeloHtmlLi(idDaPericia){
+    let periciaAEscrever = grupoDePericias[idDaPericia];
+    let valorTotalDoAtributo = document.querySelector(`#${periciaAEscrever.idAtributoBase}`).querySelector("#nomeDoAtributo").textContent;    
     let elementoPericia = document.createElement("li");
+    elementoPericia.setAttribute("id", periciaAEscrever.id);
     elementoPericia.innerHTML = `
     <div class="row">        
-            <span id="nome" class="col-3">${periciaEscrita.nome}</span>
-            <span id="atributoBase" class="col-3">${periciaEscrita.atributoBase}</span>            
+            <span id="nomeDaPericia" class="col-3">${periciaAEscrever.nome}</span>
+            <span id="atributoBase" class="col-3">${valorTotalDoAtributo}</span>            
             <span id="valorAdicional" class="col-2"><input type="number" id="valorInvestidoPericia"></span>
-            <span id="valorTotal" class="col-2">${periciaEscrita.valorTotalDaPericia}</span>
+            <span id="valorTotal" class="col-2">0</span>
             <span class="col-1"><button id="apagaPericia">Apagar</button></span>
         </div>
     `;
     return elementoPericia;
-};
-
-function renderHtmlPericia(selecionaIdListaDePericias, escreverHtmlItemPericia){
-    selecionaIdListaDePericias.append(escreverHtmlItemPericia);
-    console.log("render rodou!");
-
 }
+
+function leitorDeInputDaPericia(idDaPericia){
+    let identificadorPericia = idDaPericia;    
+    let identificadorDoInputPericia = document.querySelector("#listaDePericiasPlayer").querySelector(`#${identificadorPericia}`).querySelector("input[type=number]");        
+    identificadorDoInputPericia.addEventListener("input", function(){
+        console.log("Input da Pericia "+ idDaPericia +" foi ativado");
+    });
+};
